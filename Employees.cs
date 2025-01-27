0dd60200e04c1eb62ba79250f84d3efd3059ca10
Gustavo de Assis Cavalheiro <gustavocavalheiro0705@gmail.com>
2025-01-27 20:50:30 -0300

    Programa simples C# (estudo sobre OO)
    Exercício simples sobre encapsulamento, propeties e listas(usando função list.Find, list.Add) finalizado seguindo o projeto inicial realizado durante o curso "C# COMPLETO Programação Orientada a Objetos + Projetos".

---
+++ Employees.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace increaseSalary
+{
+    internal class Employees
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public double Salary { get; set; }
+
+        public Employees(int id, string name, double salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        public override string ToString()
+        {
+            return " ID: " + Id
+                + ", Name: " + Name
+                + ", Salary: " + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public void IncreaseSalary(double percent)
+        {
+            Salary += Salary * (percent/100);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+namespace increaseSalary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+   
+            List<Employees> employees = new List<Employees>();
+            Console.Write("How many employees will be registered: ");
+            int quantEmployees = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < quantEmployees; i++)
+            {
+                Console.WriteLine("Employee #" + (i+1) + ": ");
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Salary: ");
+                double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.WriteLine();
+                employees.Add(new Employees(id, name, salary));
+            }
+            Console.Write("Enter the employee id that will have salary increase: ");
+            int code = int.Parse(Console.ReadLine());
+            Employees idExist = employees.Find(x => x.Id == code);
+            if (idExist!= null)
+            {
+                Console.Write("Enter the percentage: ");
+                double percentage = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                idExist.IncreaseSalary(percentage);
+            }
+            else
+            {
+                Console.WriteLine("this id doesn't exist!");
+            }
+            Console.WriteLine("Updated list of employees");
+            foreach (Employees obj in employees)
+            {
+                Console.WriteLine(obj);
+            }
+
+
+        }
+    }
+}
