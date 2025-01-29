e1666a2e093f54f747f05d8d1fb08bdc4e8f5735
Gustavo de Assis Cavalheiro <gustavocavalheiro0705@gmail.com>
2025-01-29 15:46:26 -0300

    Exercício sobre matrizes em C#
    Exercício simples sobre matrizes finalizado seguindo o projeto inicial realizado durante o curso "C# COMPLETO Programação Orientada a Objetos + Projetos".

--- Program.cs
+++ Program.cs
@@ -1,48 +1,54 @@
-﻿using System.Collections.Generic;
-using System.Globalization;
-namespace increaseSalary
+﻿namespace matrixExercise
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-   
-            List<Employees> employees = new List<Employees>();
-            Console.Write("How many employees will be registered: ");
-            int quantEmployees = int.Parse(Console.ReadLine());
+            Console.Write("Enter the number of rows the matrix will have: ");
+            int m = int.Parse(Console.ReadLine());
 
-            for (int i = 0; i < quantEmployees; i++)
-            {
-                Console.WriteLine("Employee #" + (i+1) + ": ");
-                Console.Write("Id: ");
-                int id = int.Parse(Console.ReadLine());
-                Console.Write("Name: ");
-                string name = Console.ReadLine();
-                Console.Write("Salary: ");
-                double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                Console.WriteLine();
-                employees.Add(new Employees(id, name, salary));
-            }
-            Console.Write("Enter the employee id that will have salary increase: ");
-            int code = int.Parse(Console.ReadLine());
-            Employees idExist = employees.Find(x => x.Id == code);
-            if (idExist!= null)
-            {
-                Console.Write("Enter the percentage: ");
-                double percentage = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                idExist.IncreaseSalary(percentage);
-            }
-            else
-            {
-                Console.WriteLine("this id doesn't exist!");
-            }
-            Console.WriteLine("Updated list of employees");
-            foreach (Employees obj in employees)
-            {
-                Console.WriteLine(obj);
+            Console.Write("Enter the number of columns the matrix will have: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[m, n];
+
+            for (int r = 0; r < m; r++) { 
+                string[] values = Console.ReadLine().Split(' ');
+                for (int c = 0; c < n; c++)
+                {
+                    mat[r,c] = int.Parse(values[c]);
+                }
             }
 
+            Console.Write("Enter the number you want information about: ");
+            int num = int.Parse(Console.ReadLine());
 
+            for (int r = 0; r < m; r++)
+            {
+                for (int c = 0; c < n; c++)
+                {
+                    if (num == mat[r, c])
+                    {
+                        Console.WriteLine("Position: " + r + "," + c );
+                        if (c > 0)
+                        {
+                            Console.WriteLine("Left: " + mat[r, c - 1]);
+                        }
+                        if (c < n - 1)
+                        { 
+                            Console.WriteLine("Right: " + mat[r, c + 1]); 
+                        }
+                        if (r < m - 1)
+                        {
+                            Console.WriteLine("Down: " + mat[r + 1, c]);
+                        }
+                        if (r > 0)
+                        {
+                            Console.WriteLine("Up: " + mat[r - 1, c]);
+                        }
+                    }
+                }
+            }
         }
     }
 }
